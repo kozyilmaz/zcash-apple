8362aba5683de16e7c185fd266966dda0fe36e55
kozyilmaz <kazim@monolytic.com>
2017-04-04 15:20:00 +0300

    add zcash init script

---
+++ zcash/files/zcash-init.sh
@@ -0,0 +1,11 @@
+
+# excerpted from zclassic/zutil/init-mac.sh
+
+if [ ! -f "$HOME/Library/Application Support/Zcash/zcash.conf" ]; then
+	echo "Creating zcash.conf"
+	mkdir -p "$HOME/Library/Application Support/Zcash/"
+	echo "rpcuser=zcashrpc" > ~/Library/Application\ Support/Zcash/zcash.conf
+	PASSWORD=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+	echo "rpcpassword=$PASSWORD" >> "$HOME/Library/Application Support/Zcash/zcash.conf"
+	echo "Complete!"
+fi
