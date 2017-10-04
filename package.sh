38a7366f26e1a07bce5d1471b4d3286fd30c1b7f
kozyilmaz <kazim@monolytic.com>
2017-10-04 15:16:04 +0300

    package script quirks

--- package.sh
+++ package.sh
@@ -1,42 +1,49 @@
 #!/bin/sh
 
 # get version info
-git_revision_output=$(git rev-parse --verify HEAD)
-git_tag_output=$(git describe --all --exact-match $git_revision_output)
+git_rev_output=$(git rev-parse --verify HEAD)
+git_tag_output=$(git describe --all --exact-match $git_rev_output)
 git_status_output=$(git diff)
 
-# check associated tag
+# check if associated tag exist for that commit
 if [[ $git_tag_output == *"tags"* ]]; then
    git_tag_string=${git_tag_output##*/}
 else
    git_tag_string="unknown"
 fi
 
-# check for modifications
+# check for any code modifications
 if [ -n "$git_status_output" ]; then
-   git_version_string=$git_revision_output-dirty
+   git_rev_string=$git_rev_output-dirty
    git_tag_string="unknown"
 else
-   git_version_string=$git_revision_output
+   git_rev_string=$git_rev_output
 fi
 
-echo "zcash-for-macos commit version : $git_version_string"
+# pick version string (tag name, if exists or commit version)
+if [[ $git_tag_string == "unknown" ]]; then
+   release_version=$git_rev_string
+else
+   release_version=$git_tag_string
+fi
+
+echo "zcash-for-macos commit version : $git_rev_string"
 echo "zcash-for-macos tag version    : $git_tag_string"
-echo "creating 'zcash-macos-$git_tag_string' release"
+echo "creating 'zcash-macos-$release_version' release"
 
 # prepare environment
 source environment;
 export BSPJOB=16;
-export BSPINSTALL=${BSPROOT}/zcash-macos-$git_tag_string;
+export BSPINSTALL=${BSPROOT}/zcash-macos-$release_version;
 
 # build tools
 make
-echo $git_version_string > ${BSPINSTALL}/usr/local/bin/version.txt
-echo $git_tag_string    >> ${BSPINSTALL}/usr/local/bin/version.txt
-echo "Zcash for macOS ($git_tag_string) is installed to ${BSPINSTALL}"
+echo $git_rev_string  > ${BSPINSTALL}/usr/local/bin/version.txt
+echo $git_tag_string >> ${BSPINSTALL}/usr/local/bin/version.txt
+echo "Zcash for macOS ($release_version) is installed to ${BSPINSTALL}"
 
 # create tarball and checksum
-echo "creating Zcash tarball 'zcash-macos-$git_tag_string.tar.bz2' and checksum"
-tar -cjvf zcash-macos-$git_tag_string.tar.bz2 zcash-macos-$git_tag_string
-shasum -a 256 zcash-macos-$git_tag_string.tar.bz2 > zcash-macos-$git_tag_string.tar.bz2.hash
-shasum -a 256 -c zcash-macos-$git_tag_string.tar.bz2.hash
+echo "creating Zcash tarball 'zcash-macos-$release_version.tar.bz2' and checksum"
+tar -cjvf zcash-macos-$release_version.tar.bz2 zcash-macos-$release_version
+shasum -a 256 zcash-macos-$release_version.tar.bz2 > zcash-macos-$release_version.tar.bz2.hash
+shasum -a 256 -c zcash-macos-$release_version.tar.bz2.hash
